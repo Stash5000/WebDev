407aaa89e903d23e67221b55aefbf31883633693
Stash5000 <stash5000@gmail.com>
2018-01-13 12:53:32 -0800

    Add files via upload

---
+++ Lab 1/Lab1/Controllers/PersonController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            Models.Person.title = "You are a person, I am a person.";
+            Models.Person.message = "Get the picture?";
+            ViewBag.body = "This is for the first lab of the Web design class.  Here you go.";
+            return View();
+        }
+    }
+}
---
+++ Lab 1/Lab1/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab1.Models
+{
+    public static class Person
+    {
+        public static string title = "Your Title";
+        public static string message = "The Message";
+    }
+}
