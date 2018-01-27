46fb546efdca244ea03d9f8709ddc5c1591e4468
Stash5000 <stash5000@gmail.com>
2018-01-27 13:47:51 -0800

    Add files via upload

---
+++ Week 3 Lab/Week_3_Lab/Week_3_Lab/Controllers/userController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Week_3_Lab.Data.Entities;
+using Week_3_Lab.Data;
+
+namespace Week_3_Lab.Controllers
+{
+    public class userController : Controller
+    {
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(users user) {
+            if (user.email.Contains("@") && !user.email.Contains(" "))
+            {
+                database.addUser(user);
+                return RedirectToAction("UserList");
+            }
+            return RedirectToAction("Create");
+        }
+
+        [HttpGet]
+        public ActionResult UserList() {
+            return View(database.getList);
+        }
+
+        [HttpGet]
+        public ActionResult details(int id) {
+            foreach (users theUser in database.getList) {
+                if (id == theUser.id) return View(theUser);
+            }
+            return RedirectToAction("Create");
+        }
+    }
+}
---
+++ Week 3 Lab/Week_3_Lab/Week_3_Lab/Data/Entities/users.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Week_3_Lab.Data.Entities
+{
+    public class users
+    {
+        public int id { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
+        public string firstName { get; set; }
+        [Display(Name = "Middle Name")]
+        public string middleName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string lastName { get; set; }
+        [Required]
+        [Display(Name = "Email")]
+        public string email { get; set; }
+        [Required]
+        [Display(Name = "Years In School")]
+        public int yIS { get; set; }
+        [Required]
+        [Display(Name = "Description")]
+        public string description { get; set; }
+    }
+}
