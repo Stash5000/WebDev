a8cbeced7c2f4442658518aab61354bddcf480e3
Stash5000 <stash5000@gmail.com>
2018-02-17 11:47:12 -0800

    Add files via upload

---
+++ Week_5_Lab/Week_4_Lab_Joey_Dias/Repository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_5_Lab_Joey_Dias.Repository
+{
+    interface IRepository
+    {
+        void save(Data.Entities.entity e);
+        void delete(Data.Entities.entity e);
+        void edit(Data.Entities.entity e);
+        Data.Entities.entity find(int id, realRepository.type toFind);
+        IEnumerable<Data.Entities.entity> getAll(realRepository.type theType, int id = -1);
+    }
+}
---
+++ Week_5_Lab/Week_4_Lab_Joey_Dias/Repository/realRepository.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Week_5_Lab_Joey_Dias.Data.Entities;
+using Week_5_Lab_Joey_Dias.Data;
+
+namespace Week_5_Lab_Joey_Dias.Repository
+{
+    public class realRepository : IRepository
+    {
+        public enum type { USER, SPACESHIP }
+        private Database data;
+        public realRepository(Database data) {
+            this.data = data;
+        }
+        public void delete(entity e)
+        {
+            if (e is User) data.users.Remove(data.users.Find(e.id)); //Find the user and remove it
+            else data.ships.Remove(data.ships.Find(e.id)); //Find the ship and remove it
+        }
+
+        public void edit(entity e)
+        {
+            if (e is User)
+            {
+                User found = data.users.Find(e.id);
+                User newU = e as User;
+                found.fName = newU.fName;
+                found.lName = newU.lName;
+                found.email = newU.email;
+                found.age = newU.age;
+                found.phone = newU.phone;
+            }
+            else {
+                spaceship found = data.ships.Find(e.id);
+                spaceship newS = e as spaceship;
+                found.description = newS.description;
+                found.price = newS.price;
+                found.size = newS.size;
+            }
+            data.SaveChanges();
+        }
+
+        public void save(entity e)
+        {
+            if (e is User)
+            {
+                data.users.Add(e as User);
+            }
+            else data.ships.Add(e as spaceship);
+            data.SaveChanges();
+        }
+
+        public entity find(int id, type toFind)
+        {
+            if (toFind == type.USER)
+            {
+                return data.users.Find(id);
+            }
+            else return data.ships.Find(id);
+        }
+
+        public IEnumerable<entity> getAll(type theType, int id = -1)
+        {
+            if (theType == type.USER)
+            {
+                List<User> userLST = new List<User>();
+                foreach (User u in data.users) {
+                    userLST.Add(u);
+                }
+                return userLST;
+            }
+            if (id == -1) return null;
+            List<spaceship> ships = new List<spaceship>();
+            foreach (spaceship s in data.ships) {
+                if (s.userId == id) ships.Add(s);
+            }
+            return ships;
+        }
+    }
+}
