3dd84bf0c5b8456c63a2cdc05a2639689f721139
Stash5000 <stash5000@gmail.com>
2018-02-23 17:16:00 -0800

    Add files via upload

---
+++ Week_6_Lab/Week_6_Lab_Joey_Dias/Scrypts/app.js
@@ -0,0 +1,17 @@
+﻿function sum() {
+    if (validate()) {
+        var fName = document.getElementById("fName").value;
+        var lName = document.getElementById("lName").value;
+        var age = document.getElementById("age").value;
+        var summary = fName + " " + lName + ", " + age;
+        document.getElementById("summary").innerText = summary;
+    } else alert("Please fill out ALL fields");
+}
+
+function validate() {
+    var fName = document.getElementById("fName").value;
+    var lName = document.getElementById("lName").value;
+    var age = document.getElementById("age").value;
+    if (fName == "" || lName == "" || age == "") return false;
+    else return true;
+}
