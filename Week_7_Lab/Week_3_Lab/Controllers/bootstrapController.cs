69b15e19ecf6b976e03e33db35655d3faf118998
Stash5000 <stash5000@gmail.com>
2018-03-06 08:10:27 -0800

    Add files via upload

---
+++ Week_7_Lab/Week_3_Lab/Controllers/bootstrapController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Week_3_Lab.Controllers
+{
+    public class bootstrapController : Controller
+    {
+        // GET: bootstrap
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Week_7_Lab/Week_3_Lab/Data/database.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Week_7_Lab.Data.Entities;
+
+namespace Week_7_Lab.Data
+{
+    public static class database
+    {
+        private static List<users> list = new List<users>();
+        public static int id = 0;
+        public static void addUser(users user) {
+            user.id = id++;
+            list.Add(user);
+        }
+        public static List<users> getList {
+            get {
+                return list;
+            }
+        }
+    }
+}
