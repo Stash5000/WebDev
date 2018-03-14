7f1f61144e7c061ec599a59e940ce37bdd3e0b76
Stash5000 <stash5000@gmail.com>
2018-03-13 18:06:04 -0700

    Add files via upload

---
+++ Week_9_Lab/Week_4_Lab/Data/Entities/spaceship.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Week_4_Lab_Joey_Dias.Data.Entities
+{
+    public class spaceship
+    {
+        [Key]
+        public int id { get; set; }
+        public int userId { get; set; }
+        public int size { get; set; }
+        public string description { get; set; }
+        public double price { get; set; }
+    }
+}
